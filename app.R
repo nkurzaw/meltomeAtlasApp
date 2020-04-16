9e2f5b3e05846eec7559b9844ebb523ff7a474da
Nils Kurzawa <nils.kurzawa@embl.de>
2020-04-16 09:18:57 +0200

    updated reference to link to journal site

--- app.R
+++ app.R
@@ -28,7 +28,7 @@ ui <- navbarPage("Meltome Atlas",
                             
                             sidebarPanel(
                               tagList(
-                                fluidRow("This R-Shiny App allows interactive exploration of the dataset published by Jarzab et al., (2020), Nat. Methods."),
+                                fluidRow("This R-Shiny App allows interactive exploration of the dataset published by", a("Jarzab et al., (2020), Nat. Methods.", href="https://www.nature.com/articles/s41592-020-0801-4")),
                                 tags$br(),
                                 fluidRow("The different tabs lead you to exploring either the comparison of thermal proteome profiles across various species or across different human cell lines and cell types."),
                                 tags$br(),
